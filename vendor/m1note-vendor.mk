6c3c374a9e3176b6e4c55ef5910f00d6efccc398
iicc1 <ignacio.iglesias@hotmail.es>
2015-10-18 02:58:36 +0200

    Uploading vendor folder for m1note

---
+++ vendor/m1note-vendor.mk
@@ -0,0 +1,7 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m1note/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m1note/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m1note/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/meizu/m1note/vendor,system/vendor)
